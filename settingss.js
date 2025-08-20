9d87d78e934919cc75d16689aa1f3aa052b8d0ff
infoafsarmia-sketch <info.afsarmia@gmail.com>
2025-08-20 12:01:39 +0600

    settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~YrkX1Rib#_bEmLMUrDgYeVt1eG7twvz5qYKO5WIQ_pdmdOTKU_C8",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "8801606741910", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
