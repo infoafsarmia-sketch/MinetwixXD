ad22c82e03f0e5db088db3fe6c79ad68d8cc1530
infoafsarmia-sketch <info.afsarmia@gmail.com>
2025-09-03 14:28:07 +0600

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,6 +1,6 @@
 
 module.exports = {
-  SESSION_ID: "malvin~p7VAzJCD#VyqZ9hR1KrHTFUKmT1HMRDFZ2vDgFpkqGDcE5AAZDT0",  // add your Session Id here
+  SESSION_ID: "malvin~Jq9yjYBK#NXXc_RsAY7BnY0V7X8iItpzrCSz6Vndj6tal4yZx730",  // add your Session Id here
   
   OWNER_NUMBER: "8801606741910", // put your phone number here
   
